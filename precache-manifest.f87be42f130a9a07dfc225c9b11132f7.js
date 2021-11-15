82c3690ca2d34cb43b39821f268fce0db338f48b
CiscoStu <themrcisco@gmail.com>
2021-11-15 11:11:34 -0500

    Updates

---
+++ precache-manifest.f87be42f130a9a07dfc225c9b11132f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2686a541ad89046d4c0ed4b16e7814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ef43feda58ecd85605a",
+    "url": "/static/css/2.964c179a.chunk.css"
+  },
+  {
+    "revision": "a26a564a91f3b70c3567",
+    "url": "/static/css/main.1cdaf00f.chunk.css"
+  },
+  {
+    "revision": "0ef43feda58ecd85605a",
+    "url": "/static/js/2.6e192956.chunk.js"
+  },
+  {
+    "revision": "a26a564a91f3b70c3567",
+    "url": "/static/js/main.dbd683dc.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
